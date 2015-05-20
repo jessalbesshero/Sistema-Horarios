3698d42514f51ce7b30ab2181573d9089cebf22a
Jesús <speed_chucho@hotmail.com>
2015-05-20 08:16:42 -0500

    Sistema horarios versión 2

---
+++ Sistema_Horario_v2/application/views/footer/footer_vista.php
@@ -0,0 +1,14 @@
+    <footer>
+        <p>&copy; Sistema creado en la UV por estudiantes de la Licenciatura en Informática:</p>
+	<p>@AlumnosUV</p>
+    </footer>
+</body>
+</html>
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
